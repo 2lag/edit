0e2d506b5137d1f5453866e167cf21b6f213b981
2lag <aidenhelfman@gmail.com>
2023-10-22 01:00:53 -0500

    small changes

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
 todo :
 - make functionality for dropdowns then clicking
 - - file
-- - - new
-- - - open
-- - - exit
+- - - new | open | exit
 - - tools
 - - - macro
-- - - - clear
-- - - - - wipes macro storage
-- - - - record
-- - - - - store keypresses in array/vector ?
-- - - - playback
-- - - - - plays back keypresses
+- - - - clear | record | playback
 - - - mutli-cursor
 - - style
-- - - hide fps
-- - - hide caret pos
-- - - hide line count
+- - - hide fps | caret pos | line count
 - make keybinds for menu ( wnd_type -> editproc -> wm_keydown )
 - fix not being able to full screen drag on vert monitor ( wnd_drag.cpp )
 - minimize var scope | const vars | clean, optimize, and create files ( make file checklist )
--- src/wnd/funcs/wnd_menu.cpp
+++ src/wnd/funcs/wnd_menu.cpp
@@ -1,6 +1,6 @@
 #include "wnd_menu.h"
 
-#define OBJ_COUNT 6
+#define OBJ_BASE_COUNT 6
 
 const RECT o_menu_file  {  -1, 24,  50, 50 },
            i_menu_file  {   0, 25,  49, 49 },
@@ -9,10 +9,10 @@ const RECT o_menu_file  {  -1, 24,  50, 50 },
            o_menu_style { 102, 24, 152, 50 },
            i_menu_style { 102, 25, 151, 49 };
 
-bool m_hovered[ OBJ_COUNT ] = { false };
+bool m_base_hovered[ OBJ_BASE_COUNT ] = { false };
 
 void wnd_menu_draw( HDC hdc, RECT wnd_sz ) {
-  WND_MENU obj[ OBJ_COUNT ] = {
+  WND_MENU obj[ OBJ_BASE_COUNT ] = {
     wnd_menu_create( o_menu_file , COL_M_GRY, true , L""     , {        } ),
     wnd_menu_create( i_menu_file , COL_D_GRY, false, L"File" , { 12,  4 } ),
     wnd_menu_create( o_menu_tools, COL_M_GRY, true , L""     , {        } ),
@@ -23,14 +23,14 @@ void wnd_menu_draw( HDC hdc, RECT wnd_sz ) {
 
   SetBkMode( hdc, TRANSPARENT );
 
-  for( s8 idx = 0; idx < OBJ_COUNT; ++idx ) {
+  for( s8 idx = 0; idx < OBJ_BASE_COUNT; ++idx ) {
     FillRect( hdc, &obj[ idx ].r, obj[ idx ].col );
     if( obj[ idx ].next ) {
       ++idx;
       FillRect( hdc, &obj[ idx ].r, obj[ idx ].col );
     }
 
-    if( m_hovered[ idx ] )
+    if( m_base_hovered[ idx ] )
       SetTextColor( hdc, COL_L_GRY );
     else
       SetTextColor( hdc, COL_M_GRY );
@@ -51,17 +51,17 @@ void wnd_menu_hovered( MSLLHOOKSTRUCT* p_mouse ) {
   POINT cm_pos = p_mouse->pt;
   ScreenToClient( h_global, &cm_pos );
 
-  static bool was_hovered[ OBJ_COUNT ] = { false };
+  static bool was_hovered[ OBJ_BASE_COUNT ] = { false };
 
-  m_hovered[ 1 ] = (bool)PtInRect( &o_menu_file , cm_pos );
-  m_hovered[ 3 ] = (bool)PtInRect( &o_menu_tools, cm_pos );
-  m_hovered[ 5 ] = (bool)PtInRect( &o_menu_style, cm_pos );
+  m_base_hovered[ 1 ] = (bool)PtInRect( &o_menu_file , cm_pos );
+  m_base_hovered[ 3 ] = (bool)PtInRect( &o_menu_tools, cm_pos );
+  m_base_hovered[ 5 ] = (bool)PtInRect( &o_menu_style, cm_pos );
 
-  for( s32 idx = 0; idx < OBJ_COUNT; ++idx ) {
-    if( m_hovered[ idx ] == was_hovered[ idx ] )
+  for( s32 idx = 0; idx < OBJ_BASE_COUNT; ++idx ) {
+    if( m_base_hovered[ idx ] == was_hovered[ idx ] )
       continue;
     
-    was_hovered[ idx ] = m_hovered[ idx ];
+    was_hovered[ idx ] = m_base_hovered[ idx ];
 
     RECT r_invalid{};
 
